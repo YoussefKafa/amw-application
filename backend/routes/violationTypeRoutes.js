5aa901ea21287b5dea4364b8722ec62d9e8f3005
youssefkafa <youssef.kafa19941@gmail.com>
2021-10-18 16:43:02 +0300

    add start script

--- backend/routes/violationTypeRoutes.js
+++ backend/routes/violationTypeRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import { createViolationType, deleteViolationType, getViolationTypes, updateViolationType } from '../controllers/violationTypeController'
-import { admin, protect } from '../middleware/authMiddleware'
+import { createViolationType, deleteViolationType, getViolationTypes, updateViolationType } from '../controllers/violationTypeController.js'
+import { admin, protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
